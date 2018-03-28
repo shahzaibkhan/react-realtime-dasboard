a86aa897c5ecee9d9dea75184cd9d6a4bbf05d81
Shahzaib Khan <shahzaib198@gmail.com>
2018-03-29 00:36:01 +0800

    Port Issue

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ function updateData() {
   })
 }
 
-http.listen(3000, function () {
-  console.log('listening on: 3000');
+http.listen(process.env.PORT || 5000, function () {
+  console.log('listening on: 5000');
 });
